f9606fcdc962b056873807a97e8a7f3ec64ffcb7
thecsharper <si@thecsharper.com>
2023-06-28 15:34:19 +0100

    Add project files.

---
+++ ZonedTimeDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿using ZonedTimeDemo;
+
+using NodaTime;
+using NodaTime.Extensions;
+using Microsoft.Extensions.Logging;
+
+var zone = DateTimeZoneProviders.Tzdb["Europe/London"];
+
+var clock = SystemClock.Instance.InZone(zone);
+
+using ILoggerFactory loggerFactory =
+    LoggerFactory.Create(builder =>
+        builder.AddSimpleConsole(options =>
+        {
+            options.IncludeScopes = true;
+            options.SingleLine = true;
+            options.TimestampFormat = "HH:mm:ss ";
+        }));
+
+var logger = loggerFactory.CreateLogger<Program>();
+
+var demo = new ZonedDemo(zone, clock, logger);
+
+demo.MainLoop(TimeSpan.FromMilliseconds(1000));
---
+++ ZonedTimeDemo/ZonedDemo.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Logging;
+
+using NodaTime.Text;
+using NodaTime;
+
+namespace ZonedTimeDemo
+{
+    public class ZonedDemo
+    {
+        private readonly DateTimeZone _zone;
+        private readonly IClock _clock;
+        private readonly ILogger _logger;
+
+        public ZonedDemo(DateTimeZone zone, IClock clock, ILogger logger)
+        {
+            _zone = zone;
+            _clock = clock;
+            _logger = logger;
+        }
+
+        public void MainLoop(TimeSpan pollingInterval)
+        {
+            while (true)
+            {
+                var now = _clock.GetCurrentInstant();
+
+                var nowInTimeZone = now.InZone(_zone);
+
+                _logger.LogInformation("At {now} ({local} local)",
+                    InstantPattern.ExtendedIso.Format(now),
+                    ZonedDateTimePattern.GeneralFormatOnlyIso.Format(nowInTimeZone));
+
+                Thread.Sleep(pollingInterval);
+            }
+        }
+    }
+
+}
